dc6cff9f90df8a4358cedf6793a7cd6bc9a6ddd6
maki <maki@bitkings.de>
2013-04-01 23:02:27 +0200

    text converted to separate spans

---
+++ moving-text.js
@@ -0,0 +1,19 @@
+//(function () {
+
+  function scanText(elementId) {
+    var textElement = document.getElementById('text1');
+    var text = textElement.textContent.trim();
+    var textNodes = [];
+    for (var i = 0; i < text.length; i++) {
+      var span = document.createElement('span');
+      span.appendChild(document.createTextNode(text[i]));
+      textNodes.push(span);
+    }
+    while (textElement.firstChild) {
+      textElement.removeChild(textElement.firstChild);
+    }
+    textNodes.forEach(function (item) {
+      textElement.appendChild(item);
+    });
+  }
+//})();
