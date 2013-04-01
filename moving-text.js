bcaec0710080cc25bc0193c928eea951f3642079
maki <maki@bitkings.de>
2013-04-01 23:10:25 +0200

    simplified to 2 boxes prepared model

--- moving-text.js
+++ moving-text.js
@@ -1,19 +1,28 @@
-//(function () {
+function createModel() {
+  return {
+    anim: {
+      dir: 1,
+      src: [],
+      dst: []
+    }
+  };
+}
 
-  function scanText(elementId) {
-    var textElement = document.getElementById('text1');
-    var text = textElement.textContent.trim();
-    var textNodes = [];
-    for (var i = 0; i < text.length; i++) {
-      var span = document.createElement('span');
-      span.appendChild(document.createTextNode(text[i]));
-      textNodes.push(span);
-    }
-    while (textElement.firstChild) {
-      textElement.removeChild(textElement.firstChild);
-    }
-    textNodes.forEach(function (item) {
-      textElement.appendChild(item);
-    });
+function convertText2SingleElements(elementId) {
+  var textElement = document.getElementById('text1');
+  var text = textElement.textContent.trim();
+  var textNodes = [];
+  for (var i = 0; i < text.length; i++) {
+    var span = document.createElement('span');
+    span.appendChild(document.createTextNode(text[i]));
+    textNodes.push(span);
   }
-//})();
+  while (textElement.firstChild) {
+    textElement.removeChild(textElement.firstChild);
+  }
+  textNodes.forEach(function (item) {
+    textElement.appendChild(item);
+  });
+  return textNodes;
+}
+
