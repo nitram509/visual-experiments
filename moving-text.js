397232a11f432896859b958e5f7c3eedafec0027
maki <maki@bitkings.de>
2013-04-01 23:55:49 +0200

    init() done

--- moving-text.js
+++ moving-text.js
@@ -1,15 +1,48 @@
-function createModel() {
+function getCoordinates(element) {
+  if (element) {
+    return {
+      x: element.offsetLeft,
+      y: element.offsetTop
+    };
+  }
+  return {x: -1, y: -1};
+}
+
+function convertToAbsolute(element, x, y) {
+  if (element) {
+    element.style.position = 'absolute';
+    element.style.left = x + 'px';
+    element.style.top = y + 'px';
+  }
+}
+
+function createMovingTextApp() {
+
+  var _anim = {
+    dir: 1,
+    elements: [],
+    src: [],
+    dst: []
+  }
+
   return {
-    anim: {
-      dir: 1,
-      src: [],
-      dst: []
-    }
+    addSourceElements: function (elements) {
+      elements.forEach(function (item) {
+        _anim.elements.push(item);
+        _anim.src.push(getCoordinates(item));
+      });
+    },
+    addDestinationElements: function (elements) {
+      elements.forEach(function (item) {
+        _anim.dst.push(getCoordinates(item));
+      });
+    },
+    anim: _anim
   };
 }
 
 function convertText2SingleElements(elementId) {
-  var textElement = document.getElementById('text1');
+  var textElement = document.getElementById(elementId);
   var text = textElement.textContent.trim();
   var textNodes = [];
   for (var i = 0; i < text.length; i++) {
@@ -26,3 +59,32 @@ function convertText2SingleElements(elementId) {
   return textNodes;
 }
 
+function copyChildNodesInvisible(sourceId, targetId) {
+  var sourceChilds = document.getElementById(sourceId).childNodes;
+  var target = document.getElementById(targetId);
+  var textNodes = [];
+  for (var i = 0; i < sourceChilds.length; i++) {
+    var clone = sourceChilds[i].cloneNode(true)
+    clone.style.visibility = 'hidden';
+    target.appendChild(clone);
+    textNodes.push(clone);
+  }
+  return textNodes;
+}
+
+var MovingText = createMovingTextApp();
+
+function init() {
+  MovingText.addSourceElements(convertText2SingleElements('text1'));
+  MovingText.addDestinationElements(copyChildNodesInvisible('text1', 'text2'));
+  for (var i = 0; i < MovingText.anim.elements.length; i++) {
+    var e = MovingText.anim.elements[i];
+    var x = MovingText.anim.src[i].x;
+    var y = MovingText.anim.src[i].y;
+    convertToAbsolute(e, x, y);
+  }
+}
+
+function startAnimation() {
+
+}
