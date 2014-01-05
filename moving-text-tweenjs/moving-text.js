f6582bf82cbe72b0e1d54266de0adb62ee83470c
nitram509 <maki@bitkings.de>
2014-01-05 18:17:42 +0100

    cleaned up old code

--- moving-text-tweenjs/moving-text.js
+++ moving-text-tweenjs/moving-text.js
@@ -18,38 +18,31 @@ function convertToAbsolute(element, x, y) {
 
 function createMovingTextApp() {
 
-  var _anim = {
-
-    xElements: [
-      {
-        animElement: undefined,
-        x: 0,
-        y: 0
-      }
-    ],
-
-    step: 0,
-    dir: 1,
-    elements: [],
-    dstElements: [],
-    src: [],
-    dst: []
-  };
+//      {
+//        animElement: undefined,
+//        srcElement: undefined,
+//        dstElements: undefined,
+//        x: 0,
+//        y: 0
+//      }
+  var _elements = []
 
   return {
-    addSourceElements: function (elements) {
-      elements.forEach(function (item) {
-        _anim.elements.push(item);
-        _anim.src.push(getCoordinates(item));
-      });
+    setSourceElements: function (elements) {
+      var len = Math.max(_elements.length, elements.length);
+      for (var i = 0; i < len; i++) {
+        _elements[i] = _elements[i] || {};
+        _elements[i].srcElement = elements[i];
+      }
     },
-    addDestinationElements: function (elements) {
-      elements.forEach(function (item) {
-        _anim.dst.push(getCoordinates(item));
-        _anim.dstElements.push(item);
-      });
+    setDestinationElements: function (elements) {
+      var len = Math.max(_elements.length, elements.length);
+      for (var i = 0; i < len; i++) {
+        _elements[i] = _elements[i] || {};
+        _elements[i].dstElement = elements[i];
+      }
     },
-    anim: _anim
+    elements: _elements
   };
 }
 
@@ -91,55 +84,54 @@ function copyChildNodesInvisible(sourceId, targetId) {
 var MovingText = createMovingTextApp();
 
 function init() {
-  MovingText.addSourceElements(convertText2SingleElements('text1'));
-  MovingText.addDestinationElements(copyChildNodesInvisible('text1', 'text2'));
-  MovingText.anim.xElements = [];
-  for (var i = 0; i < MovingText.anim.elements.length; i++) {
-    var e = MovingText.anim.elements[i];
-    var x = MovingText.anim.src[i].x;
-    var y = MovingText.anim.src[i].y;
-    convertToAbsolute(e, x, y);
+  MovingText.setSourceElements(convertText2SingleElements('text1'));
+  MovingText.setDestinationElements(copyChildNodesInvisible('text1', 'text2'));
+  for (var i = 0; i < MovingText.elements.length; i++) {
+    var elem = MovingText.elements[i];
 
-    (function () {
-      var elem = {
-        animElement: e,
-        x: MovingText.anim.src[i].x,
-        y: MovingText.anim.src[i].y
-      }
-      MovingText.anim.xElements.push(elem);
+    var srcCoordinates = getCoordinates(elem.srcElement);
+    var dstCoordinates = getCoordinates(elem.dstElement);
+    elem.x = srcCoordinates.x;
+    elem.y = srcCoordinates.y;
+    elem.animElement = elem.srcElement;
 
-      var updateCallback = function () {
-        this.animElement.style.left = this.x + 'px';
-        this.animElement.style.top = this.y + 'px';
-      }
+    var updateCallback = function () {
+      this.animElement.style.left = this.x + 'px';
+      this.animElement.style.top = this.y + 'px';
+    }
 
-      var delay = i % ((MovingText.anim.src.length / 4) | 0) * 50;
-      var animTime = 2000;
+    var delay = 50 + ((Math.sin((Math.PI * i)/10) * 25) | 0);
+    var animTime = 1000;
 
-      var tween = new TWEEN.Tween(elem)
-         .to({
-           x: MovingText.anim.dst[i].x,
-           y: MovingText.anim.dst[i].y
-         }, animTime)
-         .delay(delay)
-         .onUpdate(updateCallback)
-         .easing(TWEEN.Easing.Sinusoidal.InOut)
-         .start();
+    var tween = new TWEEN.Tween(elem)
+       .to({
+         x: dstCoordinates.x,
+         y: dstCoordinates.y
+       }, animTime)
+       .delay(delay)
+       .onUpdate(updateCallback)
+       .easing(TWEEN.Easing.Sinusoidal.InOut)
+       .start();
 
-      var tweenBack = new TWEEN.Tween(elem)
-         .to({
-           x: MovingText.anim.src[i].x,
-           y: MovingText.anim.src[i].y
-         }, animTime)
-         .delay(delay)
-         .onUpdate(updateCallback)
-         .easing(TWEEN.Easing.Sinusoidal.InOut);
+    var tweenBack = new TWEEN.Tween(elem)
+       .to({
+         x: srcCoordinates.x,
+         y: srcCoordinates.y
+       }, animTime)
+       .delay(delay)
+       .onUpdate(updateCallback)
+       .easing(TWEEN.Easing.Sinusoidal.InOut);
 
-      tween.chain(tweenBack);
-      tweenBack.chain(tween);
-    })();
-
+    tween.chain(tweenBack);
+    tweenBack.chain(tween);
   }
+
+  (function convertAllElementsToAbsolute() {
+    for (var i = 0; i < MovingText.elements.length; i++) {
+      var elem = MovingText.elements[i];
+      convertToAbsolute(elem.srcElement, elem.x, elem.y);
+    }
+  })();
 }
 
 function animate() {
